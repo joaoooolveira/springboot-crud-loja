deb4fdc66b12b3d7abad8a18c2aa2e7c92e46876
João <oliveirapedro.joao2301@gmail.com>
2026-04-17 21:05:57 +0100

    first commit

---
+++ src/main/java/com/loja/superMuffato/SuperMuffatoApplication.java
@@ -0,0 +1,13 @@
+package com.loja.superMuffato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMuffatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMuffatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loja/superMuffato/SuperMuffatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja.superMuffato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMuffatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
